1467f4b8587c376a2a52e2c4755abd021895d909
Daniel Fairley <dfairley@researchnow.com>
2015-08-28 19:00:01 -0500

    init commit

---
+++ src/main/groovy/bartender/BartenderApplication.groovy
@@ -0,0 +1,12 @@
+package bartender
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BartenderApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run BartenderApplication, args
+    }
+}
---
+++ src/test/groovy/bartender/BartenderApplicationTests.groovy
@@ -0,0 +1,18 @@
+package bartender
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = BartenderApplication)
+@WebAppConfiguration
+class BartenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
